43224c062dedbe110d0058212a2ed83cbe26034d
Anugugan <anurohi2013@gmail.com>
2025-06-29 19:12:30 +0530

    Add files via upload

---
+++ week1/casestudy/FibonacciSeries.java
@@ -0,0 +1,19 @@
+package week1.casestudy;
+
+public class FibonacciSeries {
+
+	public static void main(String[] args) {
+		int terms=8;
+		int first=0;
+		int second=1;
+		for (int i= 1;  i<=terms ; i++) {
+			System.out.println(first + "");
+		int next = first + second;
+				first=second;
+		second=next;
+		}
+		
+
+	}
+
+}
---
+++ week1/casestudy/Library.java
@@ -0,0 +1,22 @@
+package week1.casestudy;
+
+public class Library {
+	
+	String addBook(String bookTitle)
+	{
+		System.out.println(bookTitle + ": Book added successfully");
+		return(bookTitle);
+	}
+	public void issueBook()
+	{
+	System.out.println(" Book issued successfully");
+	}
+	
+	public static void main(String[] args) {
+		Library lm=new Library();
+		lm.addBook("The Hope");
+		lm.issueBook();	
+		
+	}
+
+}
