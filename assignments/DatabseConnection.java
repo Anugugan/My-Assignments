5a73516dedd44bde8f1b063d9c23248ef54833e8
Anugugan <anurohi2013@gmail.com>
2025-07-13 23:37:48 +0530

    Delete assignments directory

--- assignments/DatabseConnection.java
+++
@@ -1,9 +0,0 @@
-package week3.assignments;
-
-public interface DatabseConnection {
-	//DAY 2 assignment 3A,3B interface 
-	public void connect();
-	public void disconnect();
-	public void executeUpdate();
-
-}
--- assignments/MySqlConnection.java
+++
@@ -1,7 +0,0 @@
-package week3.assignments;
-
-public abstract class MySqlConnection implements DatabseConnection{//day 2 assignment 3b
-	
-	public abstract void ExecuteQuery();
-
-}
