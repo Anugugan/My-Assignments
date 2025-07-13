c236257f94b3255247cb231d90ed9794b6791d59
Anugugan <anurohi2013@gmail.com>
2025-07-13 23:39:14 +0530

    Add files via upload

---
+++ week1/CaseStudy/LibraryManagement.java
@@ -0,0 +1,11 @@
+package week1.CaseStudy;
+
+public class LibraryManagement {
+
+	public static void main(String[] args) {
+		Library Lmt=new Library();
+		Lmt.addBook("A002");
+		Lmt.issueBook();
+	}
+
+}
---
+++ week1/assignment/CheckNumberIsPositive.java
@@ -0,0 +1,18 @@
+package week1.assignment;
+
+public class CheckNumberIsPositive {
+
+	public static void main(String[] args) {
+		int number=10;
+		if(number>0)
+		{System.out.println(number + "is a positive number");
+
+	}
+		else if(number<0)
+		{System.out.println(number + "is not a positive number");
+}
+		else{
+			System.out.println(number + "is zero");
+		}
+	
+	}}
